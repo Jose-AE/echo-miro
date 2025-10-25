4a9946bc9ca647dd023894217aa923f741d63425
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2025-10-25 15:16:33 +1100

    Migrate to UV package manager

--- src/main.py
+++ src/main.py
@@ -4,9 +4,7 @@ from calculate_fps import calculate_fps
 from echo_miro import EchoMiro
 
 # from get_emotion_from_frame import get_emotion_from_frame
-# from replace_background import replace_background
-
-
+# from replace_background import replace_back
 if __name__ == "__main__":
-    echo_miro = EchoMiro(camera_index=0, fullscreen=False)
+    echo_miro = EchoMiro(camera_index=0, fullscreen=True)
     echo_miro.run()
